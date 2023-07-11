7cb77f9c2b0aed49d29f9cea6e6b75e6d20af3d6
Tammy Zhang <tz332@cornell.edu>
2023-07-10 18:48:16 -0600

    reconfigure backend

--- app.py
+++ app.py
@@ -20,71 +20,34 @@ lat_array = data['y']
 data_array = data['TS']
 time_data_array = time_data['TS']
 
-# following function from ScottSyms tileshade repo under the GNU General Public License v3.0.
-# https://github.com/ScottSyms/tileshade/
-def tile2mercator(xtile, ytile, zoom):
-    # takes the zoom and tile path and passes back the EPSG:3857
-    # coordinates of the top left of the tile.
-    # From Openstreetmap
-    n = 2.0 ** zoom
-    lon_deg = xtile / n * 360.0 - 180.0
-    lat_rad = math.atan(math.sinh(math.pi * (1 - 2 * ytile / n)))
-    lat_deg = math.degrees(lat_rad)
-
-    # Convert the results of the degree calulation above and convert
-    # to meters for web map presentation
-    mercator = lnglat_to_meters(lon_deg, lat_deg)
-    return mercator
-
-# following function adapted from ScottSyms tileshade repo under the GNU General Public License v3.0.
-# https://github.com/ScottSyms/tileshade/
-# changes made: snapping values to ensure continuous tiles; use of quadmesh instead of points; syntax changes to work with Flask.
-def generateatile(zoom, x, y):
-    # The function takes the zoom and tile path from the web request,
-    # and determines the top left and bottom right coordinates of the tile.
-    # This information is used to query against the dataframe.
-    xleft, yleft = tile2mercator(int(x), int(y), int(zoom))
-    xright, yright = tile2mercator(int(x)+1, int(y)+1, int(zoom))
-
-    # ensuring no gaps are left between tiles due to partitioning occurring between coordinates.
-    # xleft_snapped = lon_array.sel(x=xleft, method="nearest").values
-    # yleft_snapped = lat_array.sel(y=yleft, method="nearest").values
-    # xright_snapped = lon_array.sel(x=xright, method="nearest").values
-    # yright_snapped = lat_array.sel(y=yright, method="nearest").values
-
-    xcondition = "x >= {xleft_snapped} and x <= {xright_snapped}".format(xleft_snapped=xleft, xright_snapped=xright)
-    ycondition = "y <= {yleft_snapped} and y >= {yright_snapped}".format(yleft_snapped=yleft, yright_snapped=yright)
-    frame = data.query(x=xcondition, y=ycondition)
-    cornerpoints = [xleft, yleft, xright, yright]
-    return [cornerpoints, frame.to_dataframe()]
-
-   
 app = Flask(__name__)
-
 
 @app.route("/")
 def index():
     return render_template('index.html', colormin = min_val, colormax = max_val)
 
-@app.route("/tiles/<int:zoom>/<int:x>/<int:y>")
-def tile(x, y, zoom):
-    results = generateatile(zoom, x, y)
+@app.route('/tiles/<ne_lng>/<ne_lat>/<sw_lng>/<sw_lat>/<year>/<forcing>')
+def get_tile(ne_lng, ne_lat, sw_lng, sw_lat, year, forcing):
+    ne_lng = float(ne_lng)
+    ne_lat = float(ne_lat)
+    sw_lng = float(sw_lng)
+    sw_lat = float(sw_lat)
+    year = int(year)
 
-    coordinates = results[0]
-    dataresults = results[1].drop(columns=['spatial_ref'])
-    
-    data = {
-        'data': dataresults.to_json(orient='table'),
-        'cornerpoints': coordinates
+    xright, yright = lnglat_to_meters(ne_lng, ne_lat)
+    xleft, yleft = lnglat_to_meters(sw_lng, sw_lat)
+
+    xcondition = "x >= {xleft} and x <= {xright}".format(xleft=xleft, xright=xright)
+    ycondition = "y <= {yright} and y >= {yleft}".format(yleft=yleft, yright=yright)
+    frame = data.query(x=xcondition, y=ycondition)
+    cornerpoints = [xleft, yleft, xright, yright]
+
+    json_data = {
+        'data': frame.to_dataframe().to_json(orient='table'),
+        'cornerpoints': cornerpoints
     }
-
-    # # convert the DataFrame to JSON
-    # json_data = dataresults.to_json(orient='table')
-
-    # # send the JSON response
-    # return jsonify(data=json_data)
-    return jsonify(data)
-
+    return jsonify(json_data)
+    
 
 @app.route('/time-series', methods=['POST'])
 def get_time_series():
