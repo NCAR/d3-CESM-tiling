b3866b1c6290a3e4de8a51ed13ff8edec6878ba2
Tammy Zhang <tz332@cornell.edu>
2023-07-11 19:23:28 -0600

    implement working time and forcing selector

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 # import libraries
+import logging
 from flask import Flask, send_file, render_template, jsonify, request
 
 import math
@@ -7,44 +8,68 @@ import xarray as xr
 
 from datashader.utils import lnglat_to_meters
 
-data = xr.open_dataset("static/data/TS2023_reproject.nc")
-time_data = xr.open_dataset("static/data/TS_annual_reproject.nc")
+data = xr.open_dataset("static/data/TS_reproject.nc")
+data_slice = ""
+
+# time_data = xr.open_dataset("static/data/TS_annual_reproject.nc")
 
 # find min/max data values to set global colorbar
 min_val = float(data['TS'].min())
 max_val = float(data['TS'].max())
 
+
 # extract dimensions
-lon_array = data['x']
-lat_array = data['y']
-data_array = data['TS']
-time_data_array = time_data['TS']
+# lon_array = data['x']
+# lat_array = data['y']
+# data_array = data['TS']
+# time_data_array = time_data['TS']
+
+def flatten_ds(year, forcing):
+    global data_slice
+    year = int(year)
+    data_slice = data.sel(time=year, forcing_type=forcing)
+    return
 
 app = Flask(__name__)
 
-@app.route("/")
-def index():
-    return render_template('index.html', colormin = min_val, colormax = max_val)
 
-@app.route('/tiles/<ne_lng>/<ne_lat>/<sw_lng>/<sw_lat>/<year>/<forcing>')
-def get_tile(ne_lng, ne_lat, sw_lng, sw_lat, year, forcing):
+app.logger.setLevel(logging.DEBUG)
+
+app.logger.debug(min_val)
+
+@app.route("/")
+def index(): 
+    flatten_ds("2023", "cmip6")
+    return render_template('index.html')
+
+@app.route("/dims/<year>/<forcing>")
+def flatten_dims(year, forcing):
+    global data_slice
+    flatten_ds(year, forcing)
+    return '', 204
+
+@app.route('/tiles/<ne_lng>/<ne_lat>/<sw_lng>/<sw_lat>')
+def get_tile(ne_lng, ne_lat, sw_lng, sw_lat):
+    global data_slice
+
     ne_lng = float(ne_lng)
     ne_lat = float(ne_lat)
     sw_lng = float(sw_lng)
     sw_lat = float(sw_lat)
-    year = int(year)
 
     xright, yright = lnglat_to_meters(ne_lng, ne_lat)
     xleft, yleft = lnglat_to_meters(sw_lng, sw_lat)
 
     xcondition = "x >= {xleft} and x <= {xright}".format(xleft=xleft, xright=xright)
     ycondition = "y <= {yright} and y >= {yleft}".format(yleft=yleft, yright=yright)
-    frame = data.query(x=xcondition, y=ycondition)
+    frame = data_slice.query(x=xcondition, y=ycondition)
     cornerpoints = [xleft, yleft, xright, yright]
 
     json_data = {
         'data': frame.to_dataframe().to_json(orient='table'),
-        'cornerpoints': cornerpoints
+        'cornerpoints': cornerpoints,
+        # 'colorlim': [float(data_slice['TS'].max()), float(data_slice['TS'].min())]
+        'colorlim': [max_val, min_val]
     }
     return jsonify(json_data)
     
