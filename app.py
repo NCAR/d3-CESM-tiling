b0b3227879d1a1d50786539258f430c0dea3cf14
Tammy Zhang <tz332@cornell.edu>
2023-07-12 16:37:53 -0600

    implement variable select

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 # import libraries
-import logging
+# import logging
 from flask import Flask, send_file, render_template, jsonify, request
 
 import math
@@ -8,49 +8,42 @@ import xarray as xr
 
 from datashader.utils import lnglat_to_meters
 
-data = xr.open_dataset("static/data/TS_reproject.nc")
-data_slice = ""
-
-# time_data = xr.open_dataset("static/data/TS_annual_reproject.nc")
-
-# find min/max data values to set global colorbar
-min_val = float(data['TS'].min())
-max_val = float(data['TS'].max())
-
-
-# extract dimensions
-# lon_array = data['x']
-# lat_array = data['y']
-# data_array = data['TS']
-# time_data_array = time_data['TS']
+CURRENT_VARIABLE = "TS"
+DATA = xr.open_dataset("static/data/TS_reproject.nc")
+DATA_SLICE = ""
 
 def flatten_ds(year, forcing):
-    global data_slice
-    year = int(year)
-    data_slice = data.sel(time=year, forcing_type=forcing)
+    global DATA
+    global DATA_SLICE
+    DATA_SLICE = DATA.sel(time=int(year), forcing_type=forcing)
     return
 
 app = Flask(__name__)
 
-
-app.logger.setLevel(logging.DEBUG)
-
-app.logger.debug(min_val)
-
 @app.route("/")
 def index(): 
+    global DATA
+    DATA = xr.open_dataset("static/data/TS_reproject.nc")
     flatten_ds("2023", "cmip6")
     return render_template('index.html')
 
+@app.route("/dataset/<variable>")
+def set_dataset(variable):
+    global DATA
+    global CURRENT_VARIABLE
+    CURRENT_VARIABLE = variable
+    DATA = xr.open_dataset("static/data/" + variable + "_reproject.nc")
+    return '', 204
+
 @app.route("/dims/<year>/<forcing>")
 def flatten_dims(year, forcing):
-    global data_slice
+    global DATA_SLICE
     flatten_ds(year, forcing)
     return '', 204
 
 @app.route('/tiles/<ne_lng>/<ne_lat>/<sw_lng>/<sw_lat>')
 def get_tile(ne_lng, ne_lat, sw_lng, sw_lat):
-    global data_slice
+    global DATA_SLICE
 
     ne_lng = float(ne_lng)
     ne_lat = float(ne_lat)
@@ -62,26 +55,24 @@ def get_tile(ne_lng, ne_lat, sw_lng, sw_lat):
 
     xcondition = "x >= {xleft} and x <= {xright}".format(xleft=xleft, xright=xright)
     ycondition = "y <= {yright} and y >= {yleft}".format(yleft=yleft, yright=yright)
-    frame = data_slice.query(x=xcondition, y=ycondition)
+    frame = DATA_SLICE.query(x=xcondition, y=ycondition)
     cornerpoints = [xleft, yleft, xright, yright]
 
     json_data = {
         'data': frame.to_dataframe().to_json(orient='table'),
         'cornerpoints': cornerpoints,
-        # 'colorlim': [float(data_slice['TS'].max()), float(data_slice['TS'].min())]
-        'colorlim': [min_val, max_val]
+        'colorlim': [float(DATA[CURRENT_VARIABLE].min()), float(DATA[CURRENT_VARIABLE].max())]
     }
     return jsonify(json_data)
     
-
 @app.route('/time-series', methods=['POST'])
 def get_time_series():
     request_data = request.get_json()
     latitude = request_data['latitude']
     longitude = request_data['longitude']
 
-    df_slice = time_data_array.sel(x=longitude, y=latitude, method="nearest")
-    df_slice = df_slice.to_dataframe().reset_index()[['year', 'TS']]
+    df_slice = DATA[CURRENT_VARIABLE].sel(x=longitude, y=latitude, method="nearest")
+    df_slice = df_slice.to_dataframe().reset_index()[['time', CURRENT_VARIABLE]]
 
     # convert the DataFrame to JSON
     json_data = df_slice.to_json(orient='records')
